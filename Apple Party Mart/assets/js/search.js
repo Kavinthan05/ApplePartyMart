c1e1b1a485f88ff9ebfbba75ba24f7a1d3cf9dca
Tharahan Muthu <tharahan@gmail.com>
2023-08-16 16:24:58 -0400

    Everything But The Search and Form

--- Apple Party Mart/assets/js/search.js
+++ Apple Party Mart/assets/js/search.js
@@ -1,15 +1,8 @@
+const searchInput = document.getElementById("searchBox");
 
-    function scrollToSection() {
-      const searchBox = document.getElementById('searchBox');
-      const searchTerm = searchBox.value.toLowerCase();
-      const sections = document.querySelectorAll('section');
-    
-      for (let i = 0; i < sections.length; i++) {
-        const section = sections[i];
-        const sectionText = section.innerText.toLowerCase();
-    
-        if (sectionText.includes(searchTerm)) {
-          section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-          break;
-        }
-      }}
+        searchInput.addEventListener("input", (e) => {
+
+            const value = e.target.value;
+            console.log(value);
+
+        });
